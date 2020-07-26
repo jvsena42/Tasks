f2ac82cecb7e6f963280806b6b97916babb89125
jvsena42 <jvsena16@gmail.com>
2020-07-26 18:24:55 -0300

    login

--- app/src/main/java/com/example/tasks/service/repository/remote/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/remote/PersonRepository.kt
@@ -1,24 +1,35 @@
 package com.example.tasks.service.repository.remote
 
+import android.content.Context
+import com.example.tasks.R
 import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PersonRepository {
+class PersonRepository (val context: Context) {
 
     private val mRemote = RetrofitClient.createService(PersonService::class.java)
 
-    fun login(email: String, password: String, listener: APIListener){
-        val call: Call<HeaderModel> = mRemote.login(email,password)
-        call.enqueue(object: Callback<HeaderModel>{
+    fun login(email: String, password: String, listener: APIListener) {
+        val call: Call<HeaderModel> = mRemote.login(email, password)
+        call.enqueue(object : Callback<HeaderModel> {
             override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
-                listener.onFailure(t.message.toString())
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
             }
 
             override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
-                response.body()?.let { listener.onSuccess(it) }
+
+                if (response.code() != TaskConstants.HTTP.SUCCESS) {
+                    val validation =
+                        Gson().fromJson(response.errorBody()!!.string(), String::class.java)
+                    listener.onFailure(validation)
+                } else {
+                    response.body()?.let { listener.onSuccess(it) }
+                }
             }
 
         })
--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,13 +2,22 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
+import com.example.tasks.service.listener.ValidationListener
+import com.example.tasks.service.repository.local.SecurityPreferences
 import com.example.tasks.service.repository.remote.PersonRepository
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val mPersonRepository = PersonRepository()
+    private val mPersonRepository = PersonRepository(application)
+    private val mSharedPerferences = SecurityPreferences(application)
+
+    private val mLogin = MutableLiveData<ValidationListener>()
+    val login : LiveData<ValidationListener> = mLogin
 
     /**
      * Faz login usando API
@@ -16,11 +25,16 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     fun doLogin(email: String, password: String) {
         mPersonRepository.login(email,password, object : APIListener{
             override fun onSuccess(model: HeaderModel) {
-                TODO("Not yet implemented")
+
+                mSharedPerferences.store(TaskConstants.SHARED.TOKEN_KEY,model.token)
+                mSharedPerferences.store(TaskConstants.SHARED.PERSON_KEY,model.personKey)
+                mSharedPerferences.store(TaskConstants.SHARED.PERSON_NAME,model.name)
+
+                mLogin.value = ValidationListener()
             }
 
             override fun onFailure(str: String) {
-                TODO("Not yet implemented")
+                mLogin.value = ValidationListener(str)
             }
 
         })
