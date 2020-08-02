416b63d23e1879c81ef959ea95085e5bf5832fad
jvsena42 <jvsena16@gmail.com>
2020-08-02 17:53:00 -0300

    edição de tarefas 2

--- app/src/main/java/com/example/tasks/service/repository/TaskRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/TaskRepository.kt
@@ -17,23 +17,23 @@ class TaskRepository(val context: Context) {
 
     private val mRemote = RetrofitClient.createService(TaskService::class.java)
 
-    fun all(listener: APIListener<List<TaskModel>>){
+    fun all(listener: APIListener<List<TaskModel>>) {
         val call: Call<List<TaskModel>> = mRemote.all()
-        list(call,listener)
+        list(call, listener)
     }
 
-    fun nextWeek(listener: APIListener<List<TaskModel>>){
+    fun nextWeek(listener: APIListener<List<TaskModel>>) {
         val call: Call<List<TaskModel>> = mRemote.nextWeek()
-        list(call,listener)
+        list(call, listener)
     }
 
-    fun overDue(listener: APIListener<List<TaskModel>>){
+    fun overDue(listener: APIListener<List<TaskModel>>) {
         val call: Call<List<TaskModel>> = mRemote.nextWeek()
-        list(call,listener)
+        list(call, listener)
     }
 
-    private fun list(call: Call<List<TaskModel>>,listener: APIListener<List<TaskModel>>){
-        call.enqueue(object : Callback<List<TaskModel>>{
+    private fun list(call: Call<List<TaskModel>>, listener: APIListener<List<TaskModel>>) {
+        call.enqueue(object : Callback<List<TaskModel>> {
             override fun onFailure(call: Call<List<TaskModel>>, t: Throwable) {
                 listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
             }
@@ -48,7 +48,8 @@ class TaskRepository(val context: Context) {
                     listener.onFailure(validation)
                 } else {
                     response.body()?.let { listener.onSuccess(it) }
-                }            }
+                }
+            }
 
         })
     }
@@ -56,6 +57,27 @@ class TaskRepository(val context: Context) {
     fun create(task: TaskModel, listener: APIListener<Boolean>) {
         val call: Call<Boolean> =
             mRemote.create(task.priorityId, task.description, task.dueDate, task.complete)
+        call.enqueue(object : Callback<Boolean> {
+            override fun onFailure(call: Call<Boolean>, t: Throwable) {
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
+            }
+
+            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
+                if (response.code() != TaskConstants.HTTP.SUCCESS) {
+                    val validation =
+                        Gson().fromJson(response.errorBody()!!.string(), String::class.java)
+                    listener.onFailure(validation)
+                } else {
+                    response.body()?.let { listener.onSuccess(it) }
+                }
+            }
+
+        })
+    }
+
+    fun update(task: TaskModel, listener: APIListener<Boolean>) {
+        val call: Call<Boolean> =
+            mRemote.update(task.id, task.priorityId, task.description, task.dueDate, task.complete)
         call.enqueue(object : Callback<Boolean> {
             override fun onFailure(call: Call<Boolean>, t: Throwable) {
                 listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
@@ -82,6 +104,51 @@ class TaskRepository(val context: Context) {
             }
 
             override fun onResponse(call: Call<TaskModel>, response: Response<TaskModel>) {
+                if (response.code() != TaskConstants.HTTP.SUCCESS) {
+                    val validation =
+                        Gson().fromJson(response.errorBody()!!.string(), String::class.java)
+                    listener.onFailure(validation)
+                } else {
+                    response.body()?.let { listener.onSuccess(it) }
+                }
+            }
+
+        })
+    }
+
+    fun updateStatus(id: Int, complete: Boolean, listener : APIListener<Boolean>) {
+
+        val call = if (complete) {
+            mRemote.complete(id)
+        } else {
+            mRemote.undo(id)
+        }
+        call.enqueue(object : Callback<Boolean> {
+            override fun onFailure(call: Call<Boolean>, t: Throwable) {
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
+            }
+
+            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
+                if (response.code() != TaskConstants.HTTP.SUCCESS) {
+                    val validation =
+                        Gson().fromJson(response.errorBody()!!.string(), String::class.java)
+                    listener.onFailure(validation)
+                } else {
+                    response.body()?.let { listener.onSuccess(it) }
+                }
+            }
+
+        })
+    }
+
+    fun delete(id: Int, listener: APIListener<Boolean>) {
+        val call: Call<Boolean> = mRemote.delete(id)
+        call.enqueue(object : Callback<Boolean> {
+            override fun onFailure(call: Call<Boolean>, t: Throwable) {
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
+            }
+
+            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
                 if (response.code() != TaskConstants.HTTP.SUCCESS) {
                     val validation =
                         Gson().fromJson(response.errorBody()!!.string(), String::class.java)
--- app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
@@ -28,4 +28,36 @@ class AllTasksViewModel(application: Application) : AndroidViewModel(application
             }
         })
     }
+
+    fun complete(id: Int){
+        updateStatus(id,true)
+    }
+
+    fun undo(id: Int){
+        updateStatus(id,false)
+    }
+
+    fun delete(id: Int){
+        mTaskRepository.delete(id, object : APIListener<Boolean>{
+            override fun onSuccess(model: Boolean) {
+                list()
+            }
+
+            override fun onFailure(str: String) {
+
+            }
+        })
+    }
+
+    fun updateStatus(id: Int, complete: Boolean){
+        mTaskRepository.updateStatus(id,complete, object : APIListener<Boolean>{
+            override fun onSuccess(model: Boolean) {
+                list()
+            }
+
+            override fun onFailure(str: String) {
+
+            }
+        })
+    }
 }
